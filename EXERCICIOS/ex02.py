d1569917028a68216bafce9500b1127326f6f8a1
Raivaz <rai.vaz@inacsistemas.com>
2024-04-09 11:17:51 -0300

    Entendendo a função format

--- EXERCICIOS/ex02.py
+++ EXERCICIOS/ex02.py
@@ -1,2 +1,4 @@
-nome = input('Digite seu nome: ')
-print('É um prazer te conhecer, {}'.format(nome))
+nome = 'Raí vaz'
+idade = 25
+peso = 68.2
+print('É um prazer te conhecer, {}. seu peso é {}, e sua idade é {} anos'.format(nome, peso, idade ))
