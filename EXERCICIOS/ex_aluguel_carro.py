11f507c909873e8d855a562f3a5098ca73ad0cff
Rai-vaz <ray.vaz04@gmail.com>
2024-04-10 06:51:38 -0300

    novo commit

--- EXERCICIOS/ex_aluguel_carro.py
+++ EXERCICIOS/ex_aluguel_carro.py
@@ -1,6 +1,8 @@
 dias = int(input('Por quantos dias foi alugado: '))
 km = float(input('Quantos km você rodou: '))
 
+teste = 'teste'
+
 total = (dias * 60) + (km * 0.15)
 
 print('O valor a pagar é R${}'.format(total))
